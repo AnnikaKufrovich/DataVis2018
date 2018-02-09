44fc0ce2c488a1ccbf6ddf8562c9708d43eb2c77
a_kufrovich <annika.kufrovich17@gmail.com>
2018-02-09 09:38:47 -0500

    Added Lab1

---
+++ Lab1/Lab1.R
@@ -0,0 +1,8 @@
+library(Lock5Data)
+head(HollywoodMovies)
+HollywoodMovies
+summary(HollywoodMovies$LeadStudio)
+
+summary(HollywoodMovies$Genre)
+summary(HollywoodMovies$Budget)
+summary(HollywoodMovies$DomesticGross)
